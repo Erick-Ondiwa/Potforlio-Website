f36195260dde9cf9a34e9bea021ad6d9572661cc
erick-ondiwa <erickochieng3860@gmail.com>
2024-07-21 22:52:30 +0300

    Modified

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,23 @@
+//function loadHTML(elementId, fileName) {
+  //   fetch(fileName)
+  //       .then(response => response.text())
+  //       .then(data => document.getElementById(elementId).innerHTML = data)
+  //       .catch(error => console.error('Error loading HTML:', error));
+  // }
+  
+  // loadHTML("header", "header.html");
+  // loadHTML("footer", "footer.html");
+
 const year = document.getElementById('year');
 const newYear = new Date().getFullYear();
 year.textContent = `${newYear}`;
 
-function loadHTML(elementId, fileName) {
-  fetch(fileName)
-      .then(response => response.text())
-      .then(data => document.getElementById(elementId).innerHTML = data)
-      .catch(error => console.error('Error loading HTML:', error));
+const first_name = document.getElementById('firstName');
+const last_name = document.getElementById('lastName');
+function getFullName(first,last){
+  const full_name = concat(first, last);
 }
-
-loadHTML("header", "header.html");
-loadHTML("footer", "footer.html");
+const phone_number = document.getElementById('telephone');
+const email_address = document.getElementById('emailAddress');
+const suggestion = document.getElementById('textArea');
+const finish_button = document.getElementById('finishButton');
