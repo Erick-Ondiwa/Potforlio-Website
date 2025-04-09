dc9e7d59c64e02cfab033d7398f9df225c9dd1ec
erick-ondiwa <erickochieng3860@gmail.com>
2025-04-09 11:55:46 +0300

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,3 @@
-//function loadHTML(elementId, fileName) {
-  //   fetch(fileName)
-  //       .then(response => response.text())
-  //       .then(data => document.getElementById(elementId).innerHTML = data)
-  //       .catch(error => console.error('Error loading HTML:', error));
-  // }
-  
-  // loadHTML("header", "header.html");
-  // loadHTML("footer", "footer.html");
-
 const year = document.getElementById('year');
 const newYear = new Date().getFullYear();
 year.textContent = `${newYear}`;
@@ -17,7 +7,3 @@ const last_name = document.getElementById('lastName');
 function getFullName(first,last){
   const full_name = concat(first, last);
 }
-const phone_number = document.getElementById('telephone');
-const email_address = document.getElementById('emailAddress');
-const suggestion = document.getElementById('textArea');
-const finish_button = document.getElementById('finishButton');
